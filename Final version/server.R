52d8d00aaa9d371389c18c983f71313f1cfcf756
Zeyu Yang <zy2327@columbia.edu>
2019-02-24 20:18:11 -0500

    update

--- Final version/server.R
+++ Final version/server.R
@@ -58,10 +58,10 @@ server <- function(input, output,session) {
   output$map <- renderLeaflet({
     leaflet() %>%
       addTiles(
-        urlTemplate = "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhcG9sZW9uIiwiYSI6ImNpa3Q0cXB5bTAwMXh2Zm0zczY1YTNkd2IifQ.rjnjTyXhXymaeYG6r2pclQ",
+        ## urlTemplate = "https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZnJhcG9sZW9uIiwiYSI6ImNpa3Q0cXB5bTAwMXh2Zm0zczY1YTNkd2IifQ.rjnjTyXhXymaeYG6r2pclQ",
         options = providerTileOptions(minZoom = 13, maxZoom = 15),
         ## Limited using for url in next line
-        ## urlTemplate = "https://api.mapbox.com/styles/v1/zy2327/cjs9914pc2grv1fphffp1vt85/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoienkyMzI3IiwiYSI6ImNqczk4ejQxejB0ZnE0NGxvZnAwMHZyMzQifQ.rut7SSkplUDV2URP5nItrw",
+        urlTemplate = "https://api.mapbox.com/styles/v1/zy2327/cjs9914pc2grv1fphffp1vt85/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoienkyMzI3IiwiYSI6ImNqczk4ejQxejB0ZnE0NGxvZnAwMHZyMzQifQ.rut7SSkplUDV2URP5nItrw",
         attribution = 'Maps by <a href="http://www.mapbox.com/">Mapbox</a>'
       ) %>%
       addCircleMarkers(data=real.time.data$station,
@@ -87,11 +87,6 @@ server <- function(input, output,session) {
   output$weather_condition <- renderText({
     paste0("Current weather condition: ",current_nyc_weather[["weather"]][[1]][["description"]])
   })
-  
-    # ## Add text of last updated time
-    # Last_update_time <- reactiveValues(update_time = real.time.data$update_time)
-    # output$update_time_Box <-renderText({
-    #   Last_update_time$update_time })
 
   
   # Add dots to map
@@ -129,9 +124,12 @@ server <- function(input, output,session) {
     #addMarkers(data=real.time.data$station,lng=real.time.data$station$lon,lat=real.time.data$station$lat)
     
     ## Add text of last updated time
-    Last_update_time <- reactiveValues(update_time = real.time.data$update_time)
+    # Last_update_time <- reactiveValues(update_time = real.time.data$update_time)
+    # output$update_time_Box <-renderText({
+    #   Last_update_time$update_time })
+    
     output$update_time_Box <-renderText({
-      Last_update_time$update_time })
+      as.character(Sys.time()) })
   })
   
   # Return nearest available stations
